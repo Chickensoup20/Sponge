ec752999678b0f126a36afc22c7967eb3a0471a1
KrastyGames <krastygamesss@gmail.com>
2026-08-27 19:41:24 +0200

    fix: ?what

--- src/main/java/org/indigo/sponge/functions/Item.java
+++ src/main/java/org/indigo/sponge/functions/Item.java
@@ -16,22 +16,21 @@ public class Item extends Sponge {
         subCategory = subCategory.toUpperCase();
         item.getItemMeta().getPersistentDataContainer().set(new NamespacedKey(plugin, "dmg"), PersistentDataType.DOUBLE, dmg);
         item.getItemMeta().getPersistentDataContainer().set(new NamespacedKey(plugin, "atkSpeed"), PersistentDataType.DOUBLE, atkSpeed);
-        if (subCategory == "Light" || subCategory == "Medium" || subCategory == "Heavy") {
+        if (subCategory.equalsIgnoreCase("Light")  || subCategory.equalsIgnoreCase("Medium") || subCategory.equalsIgnoreCase("Heavy")) {
             ItemMeta meta = item.getItemMeta();
             meta.displayName(MiniMessage.miniMessage().deserialize("<font:lore_icons>a<reset> <dark_gray>» <reset>" + name));
-            meta.setTooltipStyle(new NamespacedKey(plugin, "melee"));
+            meta.setTooltipStyle(new NamespacedKey("minecraft", "melee"));
             item.setItemMeta(meta);
-
         }
-        if (subCategory == "Ranged")
+        if (subCategory.equalsIgnoreCase("Ranged"))
         {
             ItemMeta meta = item.getItemMeta();
             meta.displayName(MiniMessage.miniMessage().deserialize("<font:lore_icons>b<reset> <dark_gray>» <reset>" + name));
-            meta.setTooltipStyle(new NamespacedKey(plugin, "ranged"));
+            meta.setTooltipStyle(new NamespacedKey("minecraft", "ranged"));
             item.setItemMeta(meta);
             item.getItemMeta().getPersistentDataContainer().set(new NamespacedKey(plugin, "maxAmmo"), PersistentDataType.DOUBLE, additionalStat);
         }
-        if (subCategory == "Magic")
+        if (subCategory.equalsIgnoreCase("Magic"))
         {
             ItemMeta meta = item.getItemMeta();
             meta.displayName(MiniMessage.miniMessage().deserialize("<font:lore_icons>c<reset> <dark_gray>» <reset>" + name));
@@ -39,7 +38,7 @@ public class Item extends Sponge {
             item.setItemMeta(meta);
             item.getItemMeta().getPersistentDataContainer().set(new NamespacedKey(plugin, "consume"), PersistentDataType.DOUBLE, additionalStat);
         }
-        if (id == "test")
+        if (id.equalsIgnoreCase("test"))
         {
             ItemMeta meta = item.getItemMeta();
             meta.setTooltipStyle(new NamespacedKey(plugin, "kitty"));
