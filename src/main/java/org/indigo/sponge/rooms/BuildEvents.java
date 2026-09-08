884405d2b414807eb6d79c3166804a1f48034cca
Chickensoup20 <niklas.worthmann@gmail.com>
2026-09-08 22:02:37 +0200

    feat: adding and removing exits, also exit orientation based on blackface

--- src/main/java/org/indigo/sponge/rooms/BuildEvents.java
+++ src/main/java/org/indigo/sponge/rooms/BuildEvents.java
@@ -3,38 +3,77 @@ package org.indigo.sponge.rooms;
 import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.util.Vector;
 import org.indigo.sponge.Sponge;
 import org.indigo.sponge.SpongePlayer;
 
+import java.util.List;
+
 public class BuildEvents implements Listener {
     boolean newEntrance = false;
+    boolean newExit = false;
+    Connector exit;
     @EventHandler
     public void interactEvent(PlayerInteractEvent event){
         if(Sponge.players.get(event.getPlayer()).getState() == SpongePlayer.State.BUILD){
             RoomTemplate room = Sponge.players.get(event.getPlayer()).getBuildingRoom();
             if(event.getItem() != null){
                 Location clickedBlock = event.getClickedBlock().getLocation();
+                event.getPlayer().sendMessage(event.getBlockFace().getDirection().toString());
                 if(event.getItem().isSimilar(Sponge.entranceWand)){
                     event.setCancelled(true);
                     Connector entrance = room.entranceConnector;
 
                     if(newEntrance == false){
-
-                        if(entrance == null){
-                            entrance = new Connector(clickedBlock,clickedBlock, Connector.ConnectorType.ENTRANCE);
-                        } else {
-                            entrance.setCorner1(clickedBlock);
+                        if(event.getAction() == Action.LEFT_CLICK_BLOCK) {
+                            if (entrance == null) {
+                                entrance = new Connector(clickedBlock, clickedBlock, Connector.ConnectorType.ENTRANCE,new Vector(1,0,0));
+                            } else {
+                                entrance.setCorner1(clickedBlock);
+                            }
+                            entrance.setCorner2(clickedBlock);
+                            entrance.spawnDisplayEntity();
+                            room.entranceConnector = entrance;
+                            newEntrance = true;
                         }
-                        entrance.setCorner2(clickedBlock);
-                        entrance.spawnDisplayEntity();
-                        room.entranceConnector = entrance;
-                        newEntrance = true;
                     } else if(newEntrance){
-                        entrance.setCorner2(event.getClickedBlock().getLocation());
-                        entrance.spawnDisplayEntity();
-                        newEntrance = false;
+                        if(event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                            entrance.setCorner2(event.getClickedBlock().getLocation());
+                            entrance.spawnDisplayEntity();
+                            newEntrance = false;
+                        }
                     }
+                }
+                if(event.getItem().isSimilar(Sponge.exitWand)){
+                    event.setCancelled(true);
+                    List<Connector> exits = room.exitConnectors;
+                    if(event.getPlayer().isSneaking()){
+                        for(Connector connector : room.exitConnectors){
+                            if(connector.boundingBox.contains(clickedBlock.getBlockX()+0.51,clickedBlock.getBlockY()+0.5,clickedBlock.getBlockZ()+0.5)){
+                                connector.removeDisplayEntity();
+                                exits.remove(connector);
+                                return;
+                            }//
+                        }
+                    }
+                    if(newExit == false){
+                        if(event.getAction() == Action.LEFT_CLICK_BLOCK) {
+                            exit = new Connector(clickedBlock, clickedBlock, Connector.ConnectorType.EXIT,event.getBlockFace().getDirection());
+                            exit.setCorner2(clickedBlock);
+                            exit.spawnDisplayEntity();
+                            newExit = true;
+                        }
+                    } else if(newExit){
+                        if(event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                            exit.setCorner2(event.getClickedBlock().getLocation());
+                            exit.spawnDisplayEntity();
+                            room.exitConnectors.add(exit);
+                            newExit = false;
+                        }
+                    }
+                    event.getPlayer().sendMessage(exit.boundingBox.toString());
                 }
             }
         }
--- src/main/java/org/indigo/sponge/rooms/Connector.java
+++ src/main/java/org/indigo/sponge/rooms/Connector.java
@@ -2,31 +2,36 @@ package org.indigo.sponge.rooms;
 
 import org.bukkit.*;
 import org.bukkit.entity.BlockDisplay;
+import org.bukkit.entity.Display;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.util.BoundingBox;
 import org.bukkit.util.Transformation;
+import org.bukkit.util.Vector;
+import org.indigo.sponge.Colors;
 import org.joml.Quaternionf;
 import org.joml.Vector3f;
 
 public class Connector {
     private World world;
-    private BoundingBox boundingBox;
+    public BoundingBox boundingBox;
     private BlockDisplay display;
     private ConnectorType type;
     private Location corner1;
     private Location corner2;
+    private Vector direction;
 
     public enum ConnectorType{
         ENTRANCE,
         EXIT
     }
-    public Connector(Location corner1, Location corner2, ConnectorType type){
+    public Connector(Location corner1, Location corner2, ConnectorType type,Vector direction){
         this.world = corner1.getWorld();
         this.corner1 = corner1;
         this.corner2 = corner2;
         boundingBox = new BoundingBox(corner1.x(),corner1.y(),corner1.z(),corner2.x(),corner2.y(),corner2.z());
         this.type = type;
+        this.direction = direction;
     }
 
     public void setCorner1(Location location){
@@ -39,16 +44,21 @@ public class Connector {
         updateBounds();
     }
 
+    public void setDirection(Vector direction){
+        this.direction = direction;
+    }
+
     private void updateBounds(){
         boundingBox.resize(corner1.x(),corner1.y(),corner1.z(),corner2.x(),corner2.y(),corner2.z());
+        boundingBox.resize(boundingBox.getMinX(), Math.floor(boundingBox.getMinY()), boundingBox.getMinZ(), boundingBox.getMaxX()+1, boundingBox.getMaxY()+1, boundingBox.getMaxZ()+1);
     }
 
     public void spawnDisplayEntity() {
         removeDisplayEntity();
         if (world == null) return;
 
-        Location min = new Location(world,boundingBox.getMinX(),boundingBox.getMinY(),boundingBox.getMinZ());
-        Location max = new Location(world,boundingBox.getMaxX(),boundingBox.getMaxY(),boundingBox.getMaxZ());
+        Location min = new Location(world,boundingBox.getMinX()-0.001,boundingBox.getMinY()-0.001,boundingBox.getMinZ()-0.001);
+        Location max = new Location(world,boundingBox.getMaxX()-0.001,boundingBox.getMaxY()-0.001,boundingBox.getMaxZ()-0.001);
 
         BlockDisplay entity = (BlockDisplay) world.spawnEntity(min, EntityType.BLOCK_DISPLAY);
         entity.setBlock(Bukkit.createBlockData(Material.GLASS));
@@ -56,12 +66,12 @@ public class Connector {
         if(type == ConnectorType.ENTRANCE) {
             entity.setGlowColorOverride(Color.ORANGE);
         } else {
-            entity.setGlowColorOverride(Color.BLUE);
+            entity.setGlowColorOverride(Color.fromRGB(Colors.SKY.red(),Colors.SKY.green(),Colors.SKY.blue()));
         }
 
-        float sizeX = (float) (max.getBlockX() - min.getBlockX() + 1);
-        float sizeY = (float) (max.getBlockY() - min.getBlockY() + 1);
-        float sizeZ = (float) (max.getBlockZ() - min.getBlockZ() + 1);
+        float sizeX = (float) (max.getBlockX() - min.getBlockX());
+        float sizeY = (float) (max.getBlockY() - min.getBlockY());
+        float sizeZ = (float) (max.getBlockZ() - min.getBlockZ());
 
         entity.setTransformation(new Transformation(
                 new Vector3f(0, 0, 0),
@@ -69,6 +79,8 @@ public class Connector {
                 new Vector3f(sizeX, sizeY, sizeZ),
                 new Quaternionf()
         ));
+
+        entity.setBrightness(new Display.Brightness(15,15));
 
         display = entity;
     }
