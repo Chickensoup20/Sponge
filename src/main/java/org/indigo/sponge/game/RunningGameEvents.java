c37be18ae461dc1b8594f7037a383d2edc0d2fd5
Chickensoup20 <niklas.worthmann@gmail.com>
2026-09-22 13:40:53 +0200

    refactor: keep running games in a Games registry

--- src/main/java/org/indigo/sponge/game/RunningGameEvents.java
+++ src/main/java/org/indigo/sponge/game/RunningGameEvents.java
@@ -4,21 +4,24 @@ import com.destroystokyo.paper.event.server.ServerTickStartEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.indigo.sponge.SpongePlayer;
 import org.indigo.sponge.game.rooms.Connector;
-
-import static org.indigo.sponge.Sponge.*;
+import org.indigo.sponge.registries.Games;
+import org.indigo.sponge.registries.Players;
 
 public class RunningGameEvents implements Listener {
     @EventHandler
     public void serverTick(ServerTickStartEvent event){
-        for(Game game : runningGames){
+        for(Game game : Games.runningGames){
             for(Connector exit : game.instances.getLast().exits){
                 if(!exit.isBlocked()){
                     int amountPlayers = game.alivePlayers.size();
                     int count = 0;
                     for(Player player : game.alivePlayers){
-                        if(exit.boundingBox.contains(player.getBoundingBox())){
-                            count++;
+                        if(Players.get(player).getState() == SpongePlayer.State.INGAME) {
+                            if (exit.boundingBox.contains(player.getBoundingBox())) {
+                                count++;
+                            }
                         }
                     }
                     if (count == amountPlayers){
---
+++ src/main/java/org/indigo/sponge/registries/Games.java
@@ -0,0 +1,11 @@
+package org.indigo.sponge.registries;
+
+import org.indigo.sponge.game.Game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Games {
+    public static List<Game> runningGames = new ArrayList<>();
+    public static boolean gameLoaded = false;
+}
